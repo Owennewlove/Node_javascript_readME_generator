2290dec3b80f81579143a63fbb5ac7d665fce67d
Owen Newlove <owen.newlove@gmail.com>
2022-07-25 14:15:22 -0400

    completed the renderLicense badge with if statements to render the licences based on the users choice

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,34 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "Apache") {  
+    return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)' 
+
+  }
+  else if (license === "Boost") {
+    return '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)'
+
+  }
+  else if (license === "Eclipse ") {
+    return '[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)'
+    
+  }
+  else if (license === "IBM") {
+    return '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)'
+    
+  }
+  else if (license === "ISC") {
+    return '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)'
+    
+  }
+  else if (license === "MIT") {
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+    
+  }
+  else {
+    return ""
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
