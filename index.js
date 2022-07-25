7dc944e38d60ef8bd0206f5fde925c9ec7c23eca
Owen Newlove <owen.newlove@gmail.com>
2022-07-25 14:08:00 -0400

    filled in the init function with inquirer prompt

--- index.js
+++ index.js
@@ -48,7 +48,16 @@ const questions = [
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    
+    inquirer.prompt(questions)
+    .then(data=>{
+        console.log(data)
+       fs.writeFileSync("./output/README.md",generateMarkdown(data) )
+    })
+
+
+}
 
 // Function call to initialize app
 init();
