1a37df181f4cd0d2b2e714a175caf4f7ae590fc1
Owen Newlove <owen.newlove@gmail.com>
2022-07-28 17:00:06 -0400

    keep changes

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+
 function renderLicenseBadge(license) {
   if (license === "Apache") {  
     return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)' 
@@ -9,7 +8,7 @@ function renderLicenseBadge(license) {
     return '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)'
 
   }
-  else if (license === "Eclipse ") {
+  else if (license === "Eclipse") {
     return '[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)'
     
   }
@@ -30,8 +29,7 @@ function renderLicenseBadge(license) {
   }
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+
 function renderLicenseLink(license) {
 
   if (license === "Apache") {  
@@ -42,7 +40,7 @@ function renderLicenseLink(license) {
     return '[Boost License](https://www.boost.org/LICENSE_1_0.txt)'
 
   }
-  else if (license === "Eclipse ") {
+  else if (license === "Eclipse") {
     return '[Eclipse License](https://opensource.org/licenses/EPL-1.0)'
     
   }
@@ -67,16 +65,17 @@ function renderLicenseLink(license) {
 
 /** this repository is licensed under the term of blank license then link to blank license */
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
+
 function renderLicenseSection(license) {
   
   return `this repository is licensed under the term of ${renderLicenseLink(license)}`
 }
 
-// TODO: Create a function to generate markdown for README
+
 function generateMarkdown(data) {
-  return `# ${data.title}  ${renderLicenseBadge(data.license)} 
+  return `# ${data.title}  ${renderLicenseBadge(data.license)}
+
+  
 
 
   ## Description
